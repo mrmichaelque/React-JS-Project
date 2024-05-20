e251250d8f46c8ac885cc6cb28d11a7637f312b3
M C Q <129240033+mrmichaelque@users.noreply.github.com>
2024-05-20 21:14:11 +0000

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: false,
-    environment: 'jsdom',
-    server.hmr.overlay: false
+  server: {
+    hmr: {
+      overlay: false,
+    },
   },
-})
+});
